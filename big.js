ae8096b8d32f30934ce8d223a3216dd682626573
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-04-25 15:16:10 -0400

    added prez

---
+++ big.js
@@ -0,0 +1,56 @@
+window.onload = function() {
+    var converter = new Showdown.converter();
+    var md = document.getElementById("md");
+    var html = converter.makeHtml(md.innerHTML);
+    md.innerHTML = html.replace(/\<(\/)?p\>/g,'<$1div>');
+    var s = document.getElementsByTagName('div'), cur = 0;
+    if (!s) return;
+    function go(n) {
+        cur = n;
+        var i = 1e3, e = s[n];
+        for (var k = 0; k < s.length; k++) s[k].style.display = 'none';
+        e.style.display = 'inline';
+        e.style.fontSize = i + 'px';
+        if (e.firstChild.nodeName === 'IMG') {
+            document.body.style.backgroundImage = 'url(' + e.firstChild.src + ')';
+            e.firstChild.style.display = 'none';
+        } else {
+            document.body.style.backgroundImage = '';
+            document.body.style.backgroundColor = e.style.backgroundColor;
+        }
+        while (
+            e.offsetWidth > window.innerWidth ||
+            e.offsetHeight > window.innerHeight) {
+            e.style.fontSize = (i -= 10) + 'px';
+            if (i < 0) break;
+        }
+        e.style.marginTop = ((window.innerHeight - e.offsetHeight) / 2) + 'px';
+        if (window.location.hash !== n) window.location.hash = n;
+        document.title = e.textContent || e.innerText;
+    }
+    document.onclick = function() {
+        go(++cur % (s.length));
+    };
+    document.onkeydown = function(e) {
+        (e.which === 39) && go(Math.min(s.length - 1, ++cur));
+        (e.which === 37) && go(Math.max(0, --cur));
+    };
+    var xStart;
+    md.addEventListener("touchstart",function(e){xStart = e.changedTouches[0].pageX}, false)
+    md.addEventListener("touchend",function(e){var test = e.changedTouches[0].pageX-xStart;
+    (test < 0) && go(Math.min(s.length - 1, ++cur));
+    (test > 0) && go(Math.max(0, --cur));
+    }, false)
+    function parse_hash() {
+        return Math.max(Math.min(
+            s.length - 1,
+            parseInt(window.location.hash.substring(1), 10)), 0);
+    }
+    if (window.location.hash) cur = parse_hash() || cur;
+    window.onhashchange = function() {
+        var c = parse_hash();
+        if (c !== cur) go(c);
+    };
+
+    go(cur);
+};
